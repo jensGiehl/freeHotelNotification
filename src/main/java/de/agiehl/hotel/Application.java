8964bcec9be8232b2167372951129d28728752f0
Jens Giehl <mobil.jgiehl@gmail.com>
2018-07-11 20:58:12 +0200

    Add POM and Application

---
+++ src/main/java/de/agiehl/hotel/Application.java
@@ -0,0 +1,14 @@
+package de.agiehl.hotel;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+
+}
